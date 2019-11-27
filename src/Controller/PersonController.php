0531be5f24997848bf79da5e49d7a670fa5329d1
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2019-11-27 13:13:26 +0100

    Lister les personnages

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -20,14 +20,43 @@ class PersonController extends AbstractController
      */
     public function person()
     {
-        return $this->render('person/persos.html.twig', [
-            'pseudo' => 'toto',
+        $p1= [
+            'nom' => 'Marc',
             'age' => 25,
+            'sexe' => true,
             'carac' => [
                 'force' => 3,
                 'agi' => 2,
                 'intel' => 3
             ]
+        ];
+        $p2= [
+            'nom' => 'Milo',
+            'age' => 30,
+            'sexe' => true,
+            'carac' => [
+                'force' => 5,
+                'agi' => 1,
+                'intel' => 2
+            ]
+        ];
+        $p3= [
+            'nom' => 'Tya',
+            'age' => 22,
+            'sexe' => false,
+            'carac' => [
+                'force' => 1,
+                'agi' => 2,
+                'intel' => 5
+            ]
+        ];
+        $players = [
+            'p1' => $p1,
+            'p2' => $p2,
+            'p3' => $p3
+        ];
+        return $this->render('person/persos.html.twig', [
+            'players' => $players
         ]);
     }
 }
